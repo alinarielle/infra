1cabfcd3cf7affc0f0aaf2a0022fd51ca953cd4b
alina arielle <hi@alina.cx>
2024-02-02 13:03:01 +0100

    pipewire works now :D

--- configuration.nix
+++ configuration.nix
@@ -62,10 +62,7 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
     audio.enable = true;
-    pulse.enable = true;
   };
 
   programs.zsh.enable = true;
