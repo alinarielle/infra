e689c80eb109e80bfba99a4408a349fc641b22f6
alina arielle <hi@alina.cx>
2024-02-14 19:06:05 +0100

    fix typos in base

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -8,7 +8,7 @@ home-manager.users.alina = {
     home.homeDirectory = "/home/alina";
     home.stateVersion = "23.11"; # To figure this out you can comment out the line and see what version it expected.
     programs.home-manager.enable = true;
-}
+};
 
 
 imports = [
