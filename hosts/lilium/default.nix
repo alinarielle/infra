04f9fcd59eae9d4d75177d4d572bdc7545ab32bd
alina arielle <hi@alina.cx>
2024-02-27 18:38:02 +0100

    refactoring, move ./profiles/base to ./common

--- hosts/lilium/default.nix
+++ hosts/lilium/default.nix
@@ -5,7 +5,7 @@
       inputs.nixos-hardware.nixosModules.lenovo-thinkpad-x1-7th-gen
       ./hardware-configuration.nix
       ../../profiles/desktop/sway
-      ../../profiles/base
+      ../../common
       ../../profiles/devel.nix
     ];
 
