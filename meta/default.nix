deeee418d392013d5b756597bce1fbf5112612a4
alina arielle <hi@alina.cx>
2024-10-26 14:44:53 +0200

    start of mkLocalMods integration

--- meta/default.nix
+++ meta/default.nix
@@ -1,12 +1,11 @@
 {
     imports = [
-        ({lib, config, pkgs, ...}: let x = (with config.l.lib; mkLocalModule ./base.nix "base profile" {}); in builtins.trace (builtins.typeOf x) x)
-	({lib, config, pkgs, ...}: builtins.trace "yep" {})
 #	./profiles
 #	./hidpi.nix
 #	./nix.nix
-	./mkLocalModule.nix
-	./enable.nix
-	./lib.nix
+#	./mkLocalModule.nix
+#	./enable.nix
+#	./lib.nix
+	./mkLocalMods.nix
     ];
 }
