fd4756af1e257c41354d4c25d11139a92a7caf2c
alina arielle <hi@alina.cx>
2024-11-02 18:46:44 +0100

    removed unnecessary audio tweaks

--- modules/profiles/hardened.nix
+++ modules/profiles/hardened.nix
@@ -1,6 +1,6 @@
 {config, ...}: with config.l.lib; {
     l.packages = enable ["noDefaultPackages"];
     l.filesystem = enable ["blacklist" "sysctl"];
-    l.network = enable ["sysctl"];
+#    l.network = enable ["sysctl"];
     l.kernel = enable ["hardened"];
 }
