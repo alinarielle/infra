cc6a17cd9845394ea4195567dd78a782109034cc
alina arielle <hi@alina.cx>
2025-03-10 12:52:02 +0100

    lilium builds

--- hosts/lilium/hardware-configuration.nix
+++ hosts/lilium/hardware-configuration.nix
@@ -10,10 +10,6 @@
       inputs.nixos-hardware.nixosModules.common-pc-ssd
     ];
   # audio fix
-  boot.kernelParams = [ "snd-intel-dspcfg.dsp_driver=1" "i8042.debug" "i8042.nopnp"];
-  boot.extraModprobeConfig = ''
-    options snd-intel-dspcfg dsp_driver=1
-  '';
   environment.systemPackages = with pkgs; [
     sof-firmware
     alsa-ucm-conf
