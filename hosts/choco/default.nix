6575beec5eb97b497e6d22f6d15067f06421e046
alina arielle <hi@alina.cx>
2025-07-16 14:06:34 +0200

    remove hosts lilium and choco because they both died

--- hosts/choco/default.nix
+++
@@ -1,9 +0,0 @@
-{config, lib, inputs, ...}: with config.l.lib; {
-  imports = [
-    inputs.nixos-hardware.nixosModules.common-pc-ssd
-    ./disko.nix
-  ];
-  l.profiles = enable ["desktop"];
-  #l.filesystem.ceph.enable = true;
-  system.stateVersion = "25.05";
-}
--- hosts/lilium/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, pkgs, inputs, lib, ... }: with config.l.lib; {
-  imports = [
-    ./hardware-configuration.nix
-  ];
-  l.profiles = enable ["gaming" "desktop"];
-  system.stateVersion = "23.11";
-}
